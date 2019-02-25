ac8f881d978df152595550346175f5a6d2f899ab
Adam Romig <adam.romig@gmail.com>
2019-02-25 09:24:06 -0500

    Completed: 7/7 - Mastering Vuex

--- vuemastery/real-world-vue/src/main.js
+++ vuemastery/real-world-vue/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store from './store/store'
 import upperFirst from 'lodash/upperFirst'
 import camelCase from 'lodash/camelCase'
 
@@ -26,5 +26,5 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
